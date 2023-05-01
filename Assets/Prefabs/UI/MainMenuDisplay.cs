1dff49e2229eba6c5c492c6d7ffeaf1d41de3146
Raymond <rayxrealms@gmail.com>
2023-05-01 01:36:23 -0400

    First commit

---
+++ Assets/Prefabs/UI/MainMenuDisplay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuDisplay : MonoBehaviour
+{
+    [SerializeField] private string gameplaySceneName = "Gameplay";
+
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameplaySceneName, LoadSceneMode.Single);
+    }
+
+    public void StartServer()
+    {
+        NetworkManager.Singleton.StartServer();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameplaySceneName, LoadSceneMode.Single);
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
