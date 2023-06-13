d426a5e7eb1b11d694bdba608337951180dfb675
kakandikonda <KaKandikondak@hotmail.com>
2023-06-12 21:32:50 -0400

    Klee Bombs Set Up

---
+++ Assets/Characters/5_Klee/MoveJumptyDumpty.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveJumptyDumpty : MonoBehaviour
+{
+
+    public KleeAbilities parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // do physics here
+    }
+
+    // collider here
+
+    // destroy game obj here
+
+
+}
