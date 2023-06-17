3347d392da47f9ae53db9ceca703bf858edce647
kakandikonda <KaKandikondak@hotmail.com>
2023-06-17 00:43:23 -0400

    Bomb physics implemented

--- Assets/Characters/5_Klee/MoveJumptyDumpty.cs
+++ Assets/Characters/5_Klee/MoveJumptyDumpty.cs
@@ -10,14 +10,14 @@ public class MoveJumptyDumpty : MonoBehaviour
     [SerializeField] private float YForce;
     [SerializeField] private float shootForce;
     private Rigidbody rb;
-    [SerializeField] private float gravity = 20f;
-    private float TempYForce = 0f;
-    private float time = 0f;
+    [SerializeField] private float gravity;
+    private float TempYForce;
+    [SerializeField] private int bounces;
     /**
      Overview of physics:
-     - add initial upward force and let the rigidbody gravity bring it down
+     - add initial upward force 
      - ball moves with a constant xz froce
-     - if ball falls below certain threshhold then reapply a force at y-force / 2
+     - if ball falls below certain threshhold then reapply the initial y-force
 
      */
 
@@ -25,33 +25,32 @@ public class MoveJumptyDumpty : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        // create the initial Y force
         rb.AddForce(new Vector3(rb.transform.forward.x * shootForce, YForce, rb.transform.forward.z * shootForce));
+        TempYForce = YForce;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // do physics here        
-        Vector3 velocity = new Vector3(rb.transform.forward.x * shootForce, VelocityFromForce(TempYForce, time), rb.transform.forward.z * shootForce);
-        //velocity.y = VelocityFromForce(TempYForce, Time.deltaTime);
-        rb.velocity = velocity;
-        TempYForce = YForce - (gravity * time);
-        Debug.Log(TempYForce);
-        time += 1;
+        // do physics here
+        // According to the Unity docs, F = m*v (force = mass * velocity) since mass = 1, velocity = force.
+        rb.velocity = new Vector3(rb.transform.forward.x * shootForce, TempYForce, rb.transform.forward.z * shootForce);
+        // decrease the force based off of how much time has passed.
+        TempYForce = TempYForce - (gravity * Time.deltaTime);
 
-
-        if (rb.position.y < 5)
+        // bounce if it y position is less than radius + .1 and if the ball can still bounce
+        if (rb.position.y < 0.71 && bounces > 0)
         {
-            YForce = YForce / 2f;
+            // increase the position so it doesnt enter multiple times
+            rb.position = new Vector3(rb.position.x, .72f, rb.position.z);
+            // reapply upward force
+            rb.AddForce(new Vector3(rb.transform.forward.x * shootForce, YForce, rb.transform.forward.z * shootForce));
+            // reset force
             TempYForce = YForce;
+            bounces -= 1;
         }
-        Debug.Log(Time.deltaTime);
 
-    }
-
-    float VelocityFromForce(float force, float time) 
-    {
-        return (force / 1) * time;
     }
     
 
